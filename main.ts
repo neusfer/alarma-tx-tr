af30a086a400e7ddb76b7cd18ec0c18cab84df92
neusfer <125355473+neusfer@users.noreply.github.com>
2023-06-01 09:34:25 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,37 +5,9 @@ radio.onReceivedNumber(function (receivedNumber) {
         estado_de_alarma = 2
     }
 })
-let alarma = 0
 let estado_de_alarma = 0
 radio.setGroup(111)
 music.setVolume(35)
 basic.forever(function () {
-    if (estado_de_alarma == 1) {
-        if (input.magneticForce(Dimension.Strength) == 150) {
-            radio.sendNumber(1)
-            alarma = 1
-            music.ringTone(262)
-            basic.showLeds(`
-                # # # # #
-                # . . . #
-                # . . . #
-                # . . . #
-                # # # # #
-                `)
-            basic.pause(10)
-            music.stopAllSounds()
-            basic.showLeds(`
-                . . . . .
-                . # # # .
-                . # . # .
-                . # # # .
-                . . . . .
-                `)
-            basic.pause(10)
-        } else {
-            radio.sendNumber(0)
-            alarma = 0
-            basic.clearScreen()
-        }
-    }
+	
 })
