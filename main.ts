2b5713e44cf61c59320c303e6bee3c8314124dc0
neusfer <125355473+neusfer@users.noreply.github.com>
2023-06-01 09:41:29 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) {
-        estado_de_alarma = 1
-    } else if (receivedNumber == 2) {
-        estado_de_alarma = 2
-    }
+    estado_de_alarma = receivedNumber
 })
 let estado_de_alarma = 0
 radio.setGroup(111)
-music.setVolume(35)
 basic.forever(function () {
-	
+    let alarma = 0
+    if (alarma == 1) {
+        basic.showIcon(IconNames.Sad)
+    } else if (alarma == 1) {
+        basic.showIcon(IconNames.Happy)
+    }
 })
