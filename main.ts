8d17607830343723e6e26697715fb4fcc8ef6682
neusfer <125355473+neusfer@users.noreply.github.com>
2023-06-08 09:10:50 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,9 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    estado_de_alarma = receivedNumber
+    alarma = receivedNumber
 })
-let estado_de_alarma = 0
+let alarma = 0
 radio.setGroup(111)
 basic.forever(function () {
-    let alarma = 0
     if (alarma == 1) {
         basic.showIcon(IconNames.Sad)
     } else if (alarma == 0) {
